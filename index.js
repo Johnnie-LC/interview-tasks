9c7544a00ad2a4ccc2046bf8d801084f365e3323
Johnnie Lopez <johesloc@gmail.com>
2021-12-29 17:10:54 -0500

    feat: import task2 file in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const task1 = require('./task1')
+const task2 = require('./task2')
 
 task1.sum((result) => console.log('task 1 -> ', result))
 task1.sum(1)((result) => console.log('task 1 -> ', result))
 task1.sum(1)(2)((result) => console.log('task 1 -> ', result))
 task1.sum(1)(2)(4)((result) => console.log('task 1 -> ', result))
+
+task2.sum(1)(2)(3)(4)(5)(6)((result) => console.log('task 2 -> ', result))
